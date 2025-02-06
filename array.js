202fdd2c65725692f90d917bae96d060bab6b658
K M Marziul Karim <marziulrafi@gmail.com>
2025-02-06 21:16:20 +0600

    initial

---
+++ array.js
@@ -0,0 +1,6 @@
+const num = [23,2,43,235,423,4]
+const fruits = ['Mango', 'Apple', 'Banana']
+const mixedArray = [13,232,'Karim', 'Messi', 123.344, 2323.432, true, false]
+
+console.log(fruits)
+console.log(mixedArray.length)
---
+++ index.js
@@ -0,0 +1,10 @@
+const num = [23,879,76,8756,678,12,90,65,80,524]
+
+console.log(num)
+console.log(num[0]) //index number
+console.log(num[3])
+console.log(num[9])
+
+
+num [1] = 123
+console.log(num)
